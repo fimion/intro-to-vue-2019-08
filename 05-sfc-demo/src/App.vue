29801f69618c8a79913431b26a473672b0bbf91f
fimion <fimion@gmail.com>
2019-08-12 21:22:09 -0400

    update sfc example

--- 05-sfc-demo/src/App.vue
+++ 05-sfc-demo/src/App.vue
@@ -1,17 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <label>
+      <input type="checkbox"
+             v-model="showComplete">
+      Show Complete
+    </label>
+    <div >
+      <h1>Alex's To-Do List</h1>
+      <ul v-if="showComplete">
+        <li v-for="item in finishedToDoItems"
+            :key="'TODO'+item.id">
+          <to-do-item :item="item"
+                      @update-item="updateItem"></to-do-item>
+        </li>
+      </ul>
+      <ul v-else>
+        <li v-for="item in unfinishedToDoItems"
+            :key="'TODO'+item.id">
+          <to-do-item :item="item"
+                      @update-item="updateItem"></to-do-item>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components:{
+    ToDoItem,
+  },
+  data(){
+    return {
+      // Do we want to see our completed todos?
+      showComplete:false,
+      // Our Illustrious To Do List
+      toDoList:[
+        {
+          id:1,
+          title:"Mow Lawn",
+          description: "It's kinda shabby looking.",
+          complete:false,
+        },
+        {
+          id:2,
+          title:"Take out the trash",
+          description: "Seriously. What is that smell?",
+          complete:false,
+        },
+        {
+          id:3,
+          title:"Take a nap.",
+          description: "You seem tired.",
+          complete:false,
+        },
+      ],
+    }
+  },
+  methods:{
+    updateItem(item){
+      this.toDoList.forEach((el, i) => {
+        if(el.id === item.id){
+          this.$set(this.toDoList, i,item);
+        }
+      });
+    },
+  },
+  computed:{
+    unfinishedToDoItems(){
+      return this.toDoList.filter(e=>!e.complete)
+    },
+    finishedToDoItems(){
+      return this.toDoList.filter(e=>e.complete)
+    }
   }
 }
 </script>
---
+++ 05-sfc-demo/src/components/ToDoItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>
+      <label>
+        <input type="checkbox"
+               @input="toggleCheckBox"
+               :checked="item.complete">
+        {{ item.title }}
+      </label>
+    </h3>
+    <p>{{ item.description }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ToDoItem",
+    props:['item'],
+    methods:{
+      toggleCheckBox(e){
+        let result = {...this.item}
+        result.complete = e.target.checked
+        this.$emit('update-item', result)
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
